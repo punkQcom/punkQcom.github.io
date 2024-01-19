89a14f094a922e698e83c344ed40f6fd6de2e347
punkQcom <marko.lahtonen@gmail.com>
2024-01-19 18:09:24 +0200

    uudestaan

--- script.js
+++ script.js
@@ -1,27 +1,11 @@
-// Move the function definition outside of any other scope
-
 var clickCounter = 0;
-var isHelloWorld = true;
 
-
-function incrementCounter() {
+function changeTextAndCount() {
     clickCounter++;
-    document.getElementById('clickButton').innerText = 'Click me (' + clickCounter + ')';
-}
 
-
-function changeText() {
-    if (isHelloWorld) {
-        // Toggle between "Hello, World!" and "Hello, GPT-3!"
-        document.getElementById('clickButton').innerText = 'Click me (0)';
-        isHelloWorld = false;
+    if (clickCounter % 2 === 1) {
+        document.getElementById('clickButton').innerText = 'Hello, World!';
     } else {
-        // Increment the counter
-        clickCounter++;
         document.getElementById('clickButton').innerText = 'Click me (' + clickCounter + ')';
-        isHelloWorld = true;
     }
-    
 }
-
-// You can add more JavaScript code here if needed
