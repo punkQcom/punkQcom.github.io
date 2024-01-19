048d4ece19bf03a89f249f58ed0bfe733f9c0242
punkQcom <marko.lahtonen@gmail.com>
2024-01-19 14:49:26 +0200

    Nappi ja functio lisätty

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 // You can add JavaScript code here if needed
+// Get the heading element by its id
+const helloTextElement = document.getElementById('helloText');
+
+// Function to change the text of the heading
+function changeText() {
+    // Change the text when the button is clicked
+    helloTextElement.textContent = 'Hello, GPT-3!';
+}
