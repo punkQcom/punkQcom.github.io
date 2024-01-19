549fa1d4ccaafe4c07a5a00d23c2fd435102d767
punkQcom <marko.lahtonen@gmail.com>
2024-01-19 20:38:26 +0200

    ei lähde

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Update Gist API URL with raw content
-const GIST_ID = 'c1a20b4538ecb4eff6ed744c8c98e94a';
-const GIST_RAW_API = `https://gist.githubusercontent.com/punkQcom/${GIST_ID}/raw/clickcounter.json`;
+const GIST_ID = 'punkQcom/c1a20b4538ecb4eff6ed744c8c98e94a';
+const GIST_RAW_API = `https://gist.githubusercontent.com/${GIST_ID}/raw/clickcounter.json`;
 
 // GitHub Personal Access Token
 const GITHUB_TOKEN = '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'; // Replace with your actual token
