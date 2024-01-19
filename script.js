7c1d5eb90aa2daa2dc73127d2be8e06705408fde
punkQcom <marko.lahtonen@gmail.com>
2024-01-19 16:49:18 +0200

    kolmas

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
 // Move the function definition outside of any other scope
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.10.2/fullcalendar.min.js"></script>
 
 function changeText() {
     // Get the heading element by its id
