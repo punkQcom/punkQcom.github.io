5d0e645c41ab4b85b992c28d353f9392d7acc9ec
punkQcom <marko.lahtonen@gmail.com>
2024-01-19 16:45:46 +0200

    liian hankala varausta osin poistettu

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 // Move the function definition outside of any other scope
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.10.2/fullcalendar.min.js"></script>
+
 function changeText() {
     // Get the heading element by its id
     const helloTextElement = document.getElementById('helloText');
