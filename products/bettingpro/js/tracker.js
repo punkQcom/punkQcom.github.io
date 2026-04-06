e7c2953d6e1e9286baab7164170d549e5d6856c2
punkQcom <marko.lahtonen@gmail.com>
2026-04-06 12:05:33 +0300

    Update BettingPro app

--- products/bettingpro/js/tracker.js
+++ products/bettingpro/js/tracker.js
@@ -3,7 +3,7 @@
  * Uses only past data to predict each match (no future data leakage).
  */
 
-import { predictMatchPure } from './prediction.js?v=1775465644';
+import { predictMatchPure } from './prediction.js?v=1775466333';
 
 const MIN_TRAINING_MATCHES = 10;
 
