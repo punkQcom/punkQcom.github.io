fda35f6903525752af8082ee9807a032fbcf3b41
punkQcom <marko.lahtonen@gmail.com>
2026-04-06 14:42:15 +0300

    Update BettingPro app

--- products/bettingpro/js/pl-simulation.js
+++ products/bettingpro/js/pl-simulation.js
@@ -3,9 +3,9 @@
  * Uses same walk-forward approach as tracker, but tracks Kelly-staked bets.
  */
 
-import { predictMatchPure, migrateOdds, getConsensusOdds } from './prediction.js?v=1775474918';
-import { shinProbabilities } from './shin.js?v=1775474918';
-import { kellyFraction } from './kelly.js?v=1775474918';
+import { predictMatchPure, migrateOdds, getConsensusOdds } from './prediction.js?v=1775475735';
+import { shinProbabilities } from './shin.js?v=1775475735';
+import { kellyFraction } from './kelly.js?v=1775475735';
 
 const MIN_TRAINING_MATCHES = 10;
 
