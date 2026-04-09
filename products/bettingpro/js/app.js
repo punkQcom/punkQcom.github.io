0a28ebf46718be77164c225a99159f31a433ccad
punkQcom <marko.lahtonen@gmail.com>
2026-04-10 00:13:20 +0300

    Update BettingPro app

--- products/bettingpro/js/app.js
+++ products/bettingpro/js/app.js
@@ -3,18 +3,18 @@
  * Predictions are precomputed on the backend; detailed analysis via /api/predict.
  */
 
-import { shinProbabilities } from './shin.js?v=1775769071';
-import { calculateEdge, kellyFraction, kellyStake } from './kelly.js?v=1775769071';
-import { buildEloTable, renderEloTable } from './elo-display.js?v=1775769071';
+import { shinProbabilities } from './shin.js?v=1775769200';
+import { calculateEdge, kellyFraction, kellyStake } from './kelly.js?v=1775769200';
+import { buildEloTable, renderEloTable } from './elo-display.js?v=1775769200';
 
-import { loadMeta, loadLeagueData, loadPreviousSeasons, loadPredictions, API_BASE } from './data-loader.js?v=1775769071';
+import { loadMeta, loadLeagueData, loadPreviousSeasons, loadPredictions, API_BASE } from './data-loader.js?v=1775769200';
 import {
   showResults, renderScoreMatrix, renderMatchOutcome,
   renderOverUnder, renderValueBets, renderAllBets, renderFades,
   renderBookmakerComparison, setupSliders, setupHelpModal,
   renderTracker, renderPLSimulation, renderTournamentFilter,
   renderMatchContext
-} from './ui.js?v=1775769071';
+} from './ui.js?v=1775769200';
 
 /** Escape HTML to prevent XSS when inserting into innerHTML/attributes. */
 function esc(str) {
